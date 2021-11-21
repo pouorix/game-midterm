ff97e979eccc94dfd6b79117909dfdb8466b9936
Pooria Parhizkar <pouorix@gmail.com>
2021-11-21 17:05:51 +0330

    Add:Move camera logic

---
+++ Assets/Scripts/CameraLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < character.transform.position.y  )
+        {
+            transform.position =
+                new Vector3(transform.position.x, character.transform.position.y, transform.position.z);
+        }
+    }
+}
