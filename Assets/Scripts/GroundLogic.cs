40db480e70d5b5c4a039a21310fb5884edb2a169
Pooria Parhizkar <pouorix@gmail.com>
2021-11-21 18:47:15 +0330

    Add:bounds

--- Assets/Scripts/GroundLogic.cs
+++ Assets/Scripts/GroundLogic.cs
@@ -19,13 +19,21 @@ public class GroundLogic : MonoBehaviour
     private void spawnGround()
     {
 
-        Vector3 vector3 = new Vector3();
-        vector3.x = UnityEngine.Random.Range(-2.5f, 2.5f);
-        vector3.y = player.transform.position.y + 3.5f;
+        Vector3 vector3_1 = new Vector3();
+        vector3_1.x = UnityEngine.Random.Range(-2.5f, 2.5f);
+        vector3_1.y = player.transform.position.y + 3.5f;
         GameObject go = Instantiate(ground);
-        go.transform.position = vector3;
+        go.transform.position = vector3_1;
         go.transform.parent = groundParents.transform;
         go.transform.localScale = new Vector3(1, 1, 1);
+
+        Vector3 vector3_2 = new Vector3();
+        vector3_2.x = UnityEngine.Random.Range(-2.5f, 2.5f);
+        vector3_2.y = player.transform.position.y + 4.5f;
+        GameObject go2 = Instantiate(ground);
+        go2.transform.position = vector3_2;
+        go2.transform.parent = groundParents.transform;
+        go2.transform.localScale = new Vector3(1, 1, 1);
     }
 
     // Update is called once per frame
